a1c647da8bf4e52ad1c71927f08ca92fa05ae75d
honggiang13 <honggiang13@gmail.com>
2017-11-24 11:08:32 +0700

    Update config.js
    change connection to mongo

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://jeffreynerona:VyxfpsiDdWh4oF1I@cluster0-shard-00-00-k7flg.mongodb.net:27017,cluster0-shard-00-01-k7flg.mongodb.net:27017,cluster0-shard-00-02-k7flg.mongodb.net:27017/mernstarter2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+  mongoURL: process.env.MONGO_URL || 'mongodb://honggiang13:abcde#12345@cluster0-shard-00-00-ljcor.mongodb.net:27017,cluster0-shard-00-01-ljcor.mongodb.net:27017,cluster0-shard-00-02-ljcor.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
   port: process.env.PORT || 8000,
 };
 
